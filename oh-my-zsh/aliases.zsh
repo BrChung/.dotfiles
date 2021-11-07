d6445fb86f55deac77ce2f2b34f764d83ce91c45
Brian Chung <35940686+BrChung@users.noreply.github.com>
2021-11-07 15:38:43 -0800

    Prefer exa over ls

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,6 +1,7 @@
 # My custom aliases for zsh
 
-alias ls='ls -lAFh'
+alias ls='exa'
+alias exa='exa -laFh --git'
 alias bbd='brew bundle dump --force --describe'
 # Prety print PATH variable (echo $PATH)
 alias trail='<<<${(F)path}'
