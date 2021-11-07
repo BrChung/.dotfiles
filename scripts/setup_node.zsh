430484da92e2f755d94cd095c37ca0fb400a8740
Brian Chung <35940686+BrChung@users.noreply.github.com>
2021-11-07 14:16:45 -0800

    Add Node Setup Script

---
+++ scripts/setup_node.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Node Setup >>>\n"
+
+# Node versions are managed with `nvm`, which is in the Brewfile.
+if [ -z "$(ls -A /usr/local/opt/nvm/versions/node)" ]; then
+    echo "No node versions found, you can install latest and continue installation by using:"
+    echo "nvm install -lts && zsh scripts/setup_node.zsh"
+    exit
+else
+    echo "Found node installation"
+fi
+
+read "response?Do you wish to install global npm packages? [Y/n] "
+response=${response:l} #tolower
+if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+
+    # Install Global NPM Packages
+    npm install -g @angular/cli
+    npm install -g hasura-cli
+
+fi
