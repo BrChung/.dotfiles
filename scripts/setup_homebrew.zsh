26e9161161821165b93f63f9a82f8f03837f9ad8
Brian Chung <brian@brianchung.co>
2024-08-26 17:14:47 -0700

    Update setup_homebrew.zsh

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -7,6 +7,7 @@ if exists brew; then
 else
   echo "brew doesn't exist, continuing with install"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
 fi
 
 # Install from Brewfile
